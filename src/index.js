ed6df6eb5cdf13bdfab2f9aed9a48bdd23f83f17
Candice Scott-Mason <cscottmason23@gmail.com>
2021-03-27 21:47:00 -0400

    Inject hobby data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 alert ("Welcome to my page!")
 
+const interest = document.getElementsByClassName('interest')[0]
+const hobby1 = interest.getElementsByTagName('li')[0]
+const hobby2 = interest.getElementsByTagName('li')[1]
+const hobby3 = interest.getElementsByTagName('li')[2]
 
+hobby1.innerText = "Basketball"
+hobby2.innerText = "Cooking"
+hobby3.innerText = "Drawing"
 
-
-/*function Submit(){
-    $("#Submit").click(function() {
-        alert("test");
-    });
-}
-
-
-
-
-/*$("#Submit").click(function() {
-    alert("Complete!");
-});
