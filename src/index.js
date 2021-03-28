bfffc3b76f316edbc40bb81d4a089c6fff952734
Candice Scott-Mason <cscottmason23@gmail.com>
2021-03-27 21:37:39 -0400

    Create hobby list

---
+++ src/index.js
@@ -0,0 +1,17 @@
+alert ("Welcome to my page!")
+
+
+
+
+/*function Submit(){
+    $("#Submit").click(function() {
+        alert("test");
+    });
+}
+
+
+
+
+/*$("#Submit").click(function() {
+    alert("Complete!");
+});
